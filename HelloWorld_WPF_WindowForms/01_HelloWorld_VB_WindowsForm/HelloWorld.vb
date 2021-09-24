af8e8993a0e30682a7b19d0e214899b430385554
Juan José Muñoz Arenas <juanjomz@iesnervion.es>
2021-09-24 12:52:41 +0200

    Add files via upload

---
+++ HelloWorld_WPF_WindowForms/01_HelloWorld_VB_WindowsForm/HelloWorld.vb
@@ -0,0 +1,10 @@
+﻿Public Class HelloWorld
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(MessageBoxButtons.OK, "Hola " + TextBox1.Text)
+    End Sub
+End Class
